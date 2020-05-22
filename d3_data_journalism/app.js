4d1f5a32fb2eb49e8f2409aa98b33b81b15c1f96
Brian <cbd.belapur.15@gmail.com>
2020-05-22 16:33:42 -0400

    update styling on circles plot and update analysis

--- d3_data_journalism/app.js
+++ d3_data_journalism/app.js
@@ -60,16 +60,32 @@ chartGroup.append("g")
 .call(leftAxis);
 
 // Create circles for chart
-
 var circlesGroup = chartGroup.selectAll("circle")
   .data(healthData)
   .enter()
   .append("circle")
   .attr("cx", d => xLinearScale(d.poverty))
   .attr("cy", d => yLinearScale(d.healthcare))
-  .attr("r", "7")
-  .attr("fill", "blue")
-  .attr("opacity", ".5");
+  .attr("r", "12")
+  .attr("fill", "green")
+  .attr("opacity", ".7");
+
+//Add state abbreviations to each circle on the chart
+var textGroup = chartGroup.append("g")
+  .selectAll("text")
+  .data(healthData)
+  .enter()
+  .append("text")
+  .attr("dx", d => xLinearScale(d.poverty)-3) //offset text to the left with -
+  .attr("dy", d => yLinearScale(d.healthcare)+2) //offset text down with +
+  .text(d => d.abbr)
+  .attr("font-family", "arial")
+  .attr("text-anchor", "middle")
+  .attr("font-size", "10px")
+  .attr("font-weight", "bold")
+  .attr("fill", "white");
+
+
 
 // Initialize tool tips
 var toolTip = d3.tip()
@@ -92,19 +108,25 @@ circlesGroup.on("click", function(data){
   toolTip.hide(data);
 });
 
+
 // Create y-axis labels
 chartGroup.append("text")
   .attr("transform", "rotate(-90)")
   .attr("y", 0 - margin.left + 40)
   .attr("x", 0 - (height / 2))
   .attr("dy", "1em")
-  .attr("class", "aText")
+  .attr("fill", "green")
+  .attr("font-weight", "bold")
+  .attr("text-anchor", "middle")
   .text("Lacks Healthcare (%)");
+
 
   // Create x-axis labels
 chartGroup.append("text")
   .attr("transform", `translate(${width /2}, ${height + margin.top + 30})`)
-  .attr("class", "aText")
+  .attr("fill", "green")
+  .attr("font-weight", "bold")
+  .attr("text-anchor", "middle")
   .text("In Poverty (%)");
 }).catch(function(error){
   console.log(error);
